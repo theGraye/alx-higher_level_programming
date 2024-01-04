79189426692508632cae140ff5c671de53cc9da2
theGraye <niimartei24@gmail.com>
2024-01-04 04:42:22 -0800

    My first toolbox!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
         print("{} argument:".format(x))
     else:
         print("{} arguments:".format(x))
-   
+
     for i in range(x):
         print("{}: {:s}".format(i + 1, argv[i + 1]))
-
